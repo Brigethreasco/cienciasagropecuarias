96314681890fb45e166109e3aeef53e8c9309f59
Brigethreasco <santosleyla115@gmail.com>
2024-11-26 15:27:53 -0500

    Ciencias agropecuarias

---
+++ src/main/java/com/mycompany/cienciasagropecuarias/Cienciasagropecuarias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cienciasagropecuarias;
+
+/**
+ *
+ * @author Personal
+ */
+public class Cienciasagropecuarias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
